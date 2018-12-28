4c5970d7597796157c7133ccf3d5389fee6717a4
Jason Franklin <jason.franklin@xing.com>
2018-12-28 13:21:39 +0100

    Generate startup names

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,35 @@ class StartupNamer extends StatelessWidget {
 }
 
 class RandomWordState extends State<RandomWord> {
+  final _suggestions = <WordPair>[];
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final _index = i ~/ 2;
+        if(_index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[_index]);
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    final _pair = WordPair.random();
-    return Center(child: Text(_pair.asPascalCase));
+    return _buildSuggestions();
   }
 
 }
