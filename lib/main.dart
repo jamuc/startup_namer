d300bb7ab2e010e39abcc8214ddc1942896ebdf7
Jason Franklin <jason.franklin@xing.com>
2018-12-28 11:52:33 +0100

    Use english words to generate random word pair

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 main(List<String> args) {
   runApp(StartupNamer());
@@ -7,13 +8,14 @@ main(List<String> args) {
 class StartupNamer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final words = WordPair.random();
     return MaterialApp(
       title: "Startup Namer",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Startup Namer"),
         ),
-        body: Center(child: Text("This is the body"),),
+        body: Center(child: Text(words.asPascalCase),),
       ),
     );
   }
